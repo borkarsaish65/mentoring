e468f031c070ac98f783980f63658148a8b23546
sumanvpacewisdom <suman.v@pacewisdom.com>
2024-09-16 11:13:47 +0530

    changes

--- src/scripts/readAndUpdateEntityTypes/readAndUpdateEntityTypes.js
+++ src/scripts/readAndUpdateEntityTypes/readAndUpdateEntityTypes.js
@@ -7,20 +7,30 @@ const rl = readline.createInterface({
 	output: process.stdout,
 })
 
-let accessToken = ''
 let adminAuthToken = ''
 let organizationId = ''
+let allowFilteringAnswer = ''
+let requiredAnswer = ''
+let entityTypeValue = ''
+let authenticatedUserToken = ''
 
-const DEFAULT_MENTORING_DOMAIN = 'http://localhost:3569'
+const DEFAULT_MENTORING_DOMAIN = 'http://localhost:3567'
 let MENTORING_DOMAIN = DEFAULT_MENTORING_DOMAIN
 
 async function main() {
 	try {
 		await promptForDomain()
-		await promptForAccessToken()
+		await promptForAuthenticatedUserToken()
+		await promptForAuthorization()
 		await promptForAdminAuthToken()
 		await promptForOrganizationId()
-		return await readAndUpdateEntityType()
+
+		const EntityTypeValues = await promptForEntityTypes()
+		const mandatoryRequired = await promptForRequired()
+		const modelValues = await promptForModelNames()
+		const allowFilterValue = await promptForAllowFiltering()
+
+		return await readAndUpdateEntityType(EntityTypeValues, allowFilterValue, modelValues, mandatoryRequired)
 	} catch (error) {
 		console.error('Error occurred:', error)
 	} finally {
@@ -36,8 +46,12 @@ async function promptForDomain() {
 	console.log(`Using domain: ${MENTORING_DOMAIN}`)
 }
 
-async function promptForAccessToken() {
-	accessToken = await promptQuestion('Enter access token: ')
+async function promptForAuthenticatedUserToken() {
+	authenticatedUserToken = await promptQuestion('Enter authenticated user token: ')
+}
+
+async function promptForAuthorization() {
+	authorization = await promptQuestion('Enter authorization token: ')
 }
 
 async function promptForAdminAuthToken() {
@@ -48,15 +62,47 @@ async function promptForOrganizationId() {
 	organizationId = await promptQuestion('Enter organization Id: ')
 }
 
-async function readAndUpdateEntityType() {
+async function promptForAllowFiltering() {
+	allowFilteringAnswer = await promptQuestion('Allow filtering? (y/n, default is n): ')
+	const allowfilteringEntitytype = allowFilteringAnswer.toLowerCase() === 'y' ? true : false
+	return allowfilteringEntitytype
+}
+
+async function promptForEntityTypes() {
+	entityTypeValue = await promptQuestion('Enter entity type value: ')
+	return [entityTypeValue]
+}
+
+async function promptForModelNames() {
+	return new Promise((resolve, reject) => {
+		rl.question('Enter model names (space separated): ', (answer) => {
+			const modelNames = answer.split(' ').filter((name) => name.length > 0)
+			if (modelNames.length > 0) {
+				console.log('Chosen model names:', modelNames)
+				resolve(modelNames)
+			} else {
+				reject(new Error('No model names provided.'))
+			}
+		})
+	})
+}
+
+async function promptForRequired() {
+	requiredAnswer = await promptQuestion('Madatory entityType ? (y/n, default is n): ')
+	const requiredEntitytype = requiredAnswer.toLowerCase() === 'y' ? true : false
+	return requiredEntitytype
+}
+
+async function readAndUpdateEntityType(readEntityType, allowFilterValue, modelValues, mandatoryRequired) {
 	try {
 		// Reading EntityType
 		const response = await axios.post(
 			`${MENTORING_DOMAIN}/mentoring/v1/entity-type/read`,
-			JSON.stringify({ value: ['status', 'medium'] }),
+			JSON.stringify({ value: readEntityType }),
 			{
 				headers: {
-					'x-auth-token': `bearer ${accessToken}`,
+					'x-authenticated-user-token': `${authenticatedUserToken}`,
+					Authorization: `bearer ${authorization}`,
 					'Content-Type': 'application/json',
 					'admin-auth-token': `${adminAuthToken}`,
 					'organization-id': `${organizationId}`,
@@ -64,44 +110,35 @@ async function readAndUpdateEntityType() {
 			}
 		)
 
+		const entityTypeResult = response.data.result.entity_types
 		const getEntitiesByType = (typeValue) => {
-			const entityType = response.data.result.entity_types.find((e) => e.value === typeValue)
+			const entityType = entityTypeResult.find((e) => e.value === typeValue)
 			return entityType ? entityType.id : []
 		}
 
-		const statusEntities = await getEntitiesByType('status')
-		const mediumEntities = await getEntitiesByType('medium')
+		for (let i = 0; i < entityTypeResult.length; i++) {
+			const entitytypebyvalue = await getEntitiesByType(entityTypeResult[i].value)
 
-		// Status Update EntityType
-		const statusEntityUpdate = await axios.post(
-			`${MENTORING_DOMAIN}/mentoring/v1/entity-type/update/${statusEntities}`,
-			JSON.stringify({ allow_filtering: false, model_names: ['Session'] }),
-			{
-				headers: {
-					'x-auth-token': `bearer ${accessToken}`,
-					'Content-Type': 'application/json',
-					'admin-auth-token': `${adminAuthToken}`,
-					'organization-id': `${organizationId}`,
-				},
-			}
-		)
+			const EntityUpdate = await axios.post(
+				`${MENTORING_DOMAIN}/mentoring/v1/entity-type/update/${entitytypebyvalue}`,
+				JSON.stringify({
+					allow_filtering: allowFilterValue,
+					model_names: modelValues,
+					required: mandatoryRequired,
+				}),
+				{
+					headers: {
+						'x-authenticated-user-token': `${authenticatedUserToken}`,
+						Authorization: `bearer ${authorization}`,
+						'Content-Type': 'application/json',
+						'admin-auth-token': `${adminAuthToken}`,
+						'organization-id': `${organizationId}`,
+					},
+				}
+			)
 
-		// Medium Update EntityType
-		const mediumEntityUpdate = await axios.post(
-			`${MENTORING_DOMAIN}/mentoring/v1/entity-type/update/${mediumEntities}`,
-			JSON.stringify({ required: true, model_names: ['Session'] }),
-			{
-				headers: {
-					'x-auth-token': `bearer ${accessToken}`,
-					'Content-Type': 'application/json',
-					'admin-auth-token': `${adminAuthToken}`,
-					'organization-id': `${organizationId}`,
-				},
-			}
-		)
-
-		console.log('Status', statusEntityUpdate.data.message)
-		console.log('Medium', mediumEntityUpdate.data.message)
+			console.log(entityTypeResult[i].value, EntityUpdate.data.message)
+		}
 	} catch (error) {
 		console.error('Entity type creation failed:', error)
 		throw error
