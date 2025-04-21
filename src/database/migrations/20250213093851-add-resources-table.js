7589bc42200742bccf168f2f16f15307e7e867dd
rakeshSgr <rakesh.k@pacewisdom.com>
2025-04-21 19:57:56 +0530

    resource schema migration

--- src/database/migrations/20250213093851-add-resources-table.js
+++ src/database/migrations/20250213093851-add-resources-table.js
@@ -53,11 +53,6 @@ module.exports = {
 	},
 
 	async down(queryInterface, Sequelize) {
-		/**
-		 * Add reverting commands here.
-		 *
-		 * Example:
-		 * await queryInterface.dropTable('users');
-		 */
+		await queryInterface.dropTable('resources')
 	},
 }
