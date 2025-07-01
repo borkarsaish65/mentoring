e702433ef7f9a78720c625783b5443af4811e504
rakeshSgr <rakesh.k@pacewisdom.com>
2025-07-01 10:19:58 +0530

    authenticator update

--- src/middlewares/authenticator.js
+++ src/middlewares/authenticator.js
@@ -119,7 +119,6 @@ module.exports = async function (req, res, next) {
 				? req.decodedToken?.organization_id?.toString()
 				: req.decodedToken?.organization_id
 
-		console.log(' req decoded tokenen ', req.decodedToken)
 		if (!req.decodedToken[organizationKey]) {
 			throw createUnauthorizedResponse()
 		}
