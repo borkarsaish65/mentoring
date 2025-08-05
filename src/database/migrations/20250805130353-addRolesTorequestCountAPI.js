776af187f62aeba606230993fcf20c71cad50f9d
Rocky <rakesh.rockysgr@gmail.com>
2025-08-05 18:44:24 +0530

    api permission added for requestCount API

---
+++ src/database/migrations/20250805130353-addRolesTorequestCountAPI.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const userRequests = require('@requests/user')
+require('dotenv').config()
+const common = require('@constants/common')
+const Permissions = require('@database/models/index').Permission
+
+const getPermissionId = async (module, request_type, api_path) => {
+	try {
+		const permission = await Permissions.findOne({
+			where: { module, request_type, api_path },
+		})
+
+		if (!permission) {
+			throw 'no permission found'
+		}
+		return permission.id
+	} catch (error) {
+		throw error
+	}
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const rolePermissionsData = [
+			{
+				role_title: common.MENTOR_ROLE,
+				permission_id: await getPermissionId('users', ['GET'], '/mentoring/v1/users/requestCount'),
+				module: 'users',
+				request_type: ['GET'],
+				api_path: 'mentoring/v1/users/requestCount',
+				created_at: new Date(),
+				updated_at: new Date(),
+				created_by: 0,
+			},
+		]
+		await queryInterface.bulkInsert('role_permission_mapping', rolePermissionsData)
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete('role_permission_mapping', null, {})
+	},
+}
