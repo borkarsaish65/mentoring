e9d218483e5e17955dd50a3b088d034d06720789
Rocky <rakesh.rockysgr@gmail.com>
2025-09-11 15:51:33 +0530

    kb search issue

--- src/generics/materializedViews.js
+++ src/generics/materializedViews.js
@@ -4,8 +4,9 @@ const { sequelize } = require('@database/models/index')
 const utils = require('@generics/utils')
 const common = require('@constants/common')
 const { getDefaultOrgId } = require('@helpers/getDefaultOrgId')
-const searchConfig = require('@configs/search.json')
+const defaultSearchConfig = require('@configs/search.json')
 const indexQueries = require('@generics/mViewsIndexQueries')
+const searchConfig = require('@root/config.json')
 
 let refreshInterval
 const groupByModelNames = async (entityTypes) => {
