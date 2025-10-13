dbbbf35534d023898d075d4d6a66d48cbb1320e4
Rocky <rakesh.rockysgr@gmail.com>
2025-10-13 16:10:15 +0530

    migration changes

--- src/database/migrations/20251011152421-updateSessionEmailTemplate.js
+++ src/database/migrations/20251011152421-updateSessionEmailTemplate.js
@@ -1,5 +1,4 @@
 'use strict'
-const moment = require('moment')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -7,7 +6,7 @@ module.exports = {
 		let notificationTemplateData = [
 			{
 				body: '<div><p>Dear {name},</p><p>I trust this email finds you well. I wanted to inform you that there have been some updates to the mentoring session previously scheduled in which you were enrolled. Please review the changes outlined below:</p><p><strong>Revised Session Details:</strong></p><ul><li><strong>Date:</strong> {newStartDate}</li><li><strong>Time:</strong> {newStartTime}</li><li><strong>Duration:</strong> {newSessionDuration} {unitOfTime}</li><li><strong>Session Platform:</strong> {newSessionPlatform}</li><li><strong>Topic:</strong> {revisedSessionTitle}</li><li><strong>Session Type:</strong> {newSessionType}</li></ul><p>We understand that schedule changes may impact your availability, and we appreciate your flexibility. If there are any concerns or conflicts with the revised schedule, please let us know as soon as possible so that we can address them accordingly. Thank you for your understanding and continued participation in the mentoring program. Your engagement contributes significantly to the success of our mentoring initiatives, and we value your ongoing support.</p></div>',
-				updated_at: moment().format(),
+				updated_at: Sequelize.literal('CURRENT_TIMESTAMP'),
 			},
 		]
 
