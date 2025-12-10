c0a81851bf5b1de4c11a12b2dd27f09f3e38d2b9
borkarsaish65 <borkarsaish65@gmail.com>
2025-12-10 14:46:48 +0530

    updated migrations

---
+++ src/scripts/run-migrations.js
@@ -0,0 +1,25 @@
+const { Umzug, SequelizeStorage } = require('umzug')
+const { sequelize } = require('../src/models') // <-- this is where Sequelize is initialized
+
+async function run() {
+	try {
+		console.log('Starting migrations...')
+
+		const umzug = new Umzug({
+			migrations: { glob: 'src/migrations/*.js' },
+			context: sequelize.getQueryInterface(),
+			storage: new SequelizeStorage({ sequelize }),
+			logger: console,
+		})
+
+		await umzug.up() // <-- THIS is the real equivalent of `sequelize-cli db:migrate`
+
+		console.log('MIGRATION_SUCCESS')
+		process.exit(0)
+	} catch (err) {
+		console.error(err)
+		process.exit(1)
+	}
+}
+
+run()
