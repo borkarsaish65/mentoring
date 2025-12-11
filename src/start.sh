4ccdbebd6825147c4ac0a7ef086495a22769133a
borkarsaish65 <borkarsaish65@gmail.com>
2025-12-11 15:47:22 +0530

    script for migration script

--- src/start.sh
+++ src/start.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
 
+# Replace kafka.js with kafka.ci.js
+cp /var/src/configs/kafka.ci.js /var/src/configs/kafka.js
+
 echo "[MENTORING] Running migrations..."
-npm run db:init
+npm run db:init:integration
 
 echo "[MENTORING] Running seeds..."
-npm run db:seed:all
+npm run db:seed:all:integration
 
 echo "[MENTORING] Starting application..."
 npm run dev
