db6f603fa0bc436bb186a7b2f61d976fe7461024
borkarsaish65 <borkarsaish65@gmail.com>
2026-01-05 18:02:08 +0530

    new fix

--- src/start.sh
+++ src/start.sh
@@ -4,11 +4,11 @@ set -e
 # Replace kafka.js with kafka.ci.js
 cp /var/src/configs/kafka.ci.js /var/src/configs/kafka.js
 
-echo "[MENTORING] Running migrations..."
-npm run db:init:integration
+# echo "[MENTORING] Running migrations..."
+# npm run db:init:integration
 
-echo "[MENTORING] Running seeds..."
-npm run db:seed:all:integration
+# echo "[MENTORING] Running seeds..."
+# npm run db:seed:all:integration
 
 echo "[MENTORING] Starting application..."
 npm run dev
