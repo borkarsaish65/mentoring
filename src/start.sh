9d0b5ff00726243c7c2095c1562de43c4006c331
borkarsaish65 <borkarsaish65@gmail.com>
2026-01-05 23:47:34 +0530

    fix-2

--- src/start.sh
+++ src/start.sh
@@ -10,6 +10,7 @@ cp /var/src/configs/kafka.ci.js /var/src/configs/kafka.js
 # echo "[MENTORING] Running seeds..."
 # npm run db:seed:all:integration
 # running script
+# running script 2
 
 echo "[MENTORING] Starting application..."
 npm run dev
