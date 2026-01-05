82f2a1b648361348f48752625dad041b4a69c13c
borkarsaish65 <borkarsaish65@gmail.com>
2026-01-06 00:16:25 +0530

    fix-3

--- src/start.sh
+++ src/start.sh
@@ -4,13 +4,11 @@ set -e
 # Replace kafka.js with kafka.ci.js
 cp /var/src/configs/kafka.ci.js /var/src/configs/kafka.js
 
-# echo "[MENTORING] Running migrations..."
-# npm run db:init:integration
+echo "[MENTORING] Running migrations..."
+npm run db:init:integration
 
-# echo "[MENTORING] Running seeds..."
-# npm run db:seed:all:integration
-# running script
-# running script 2
+echo "[MENTORING] Running seeds..."
+npm run db:seed:all:integration
 
 echo "[MENTORING] Starting application..."
 npm run dev
